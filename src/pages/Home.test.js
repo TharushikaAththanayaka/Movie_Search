fdd101299580c5a7a35d43fd71e5d32a8ff34d37
Tharushika Aththanayaka <tharushika.aththanayaka@pronto.net>
2025-06-25 15:31:01 +0530

    Add test cases for NavBar, Home, and Movie components

---
+++ src/pages/Home.test.js
@@ -0,0 +1,31 @@
+import {render , screen} from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+import Home from "./Home";
+
+test("renders the home page paragraph" , () =>{
+   render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+    const paragraphEl = screen.getByText(/Match Your Mood/i);
+    expect(paragraphEl).toBeInTheDocument();
+});
+
+test("renders the explore more button" ,async() =>{
+   const handleExplore = jest.fn();
+   
+   render(
+   <BrowserRouter>
+   <button onClick={handleExplore}>Explore More</button>
+   </BrowserRouter>
+   );
+   const buttonEl = screen.getByRole('button' , {name : /explore more/i });
+
+   await userEvent.click(buttonEl);
+   expect(handleExplore).toHaveBeenCalledTimes(1);
+
+})
+
